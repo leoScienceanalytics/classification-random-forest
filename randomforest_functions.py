2949a9da4c499384b33fe3ce9439f00a43e54e6b
Leonam Alves <leon14alv@gmail.com>
2024-03-20 21:56:05 -0300

    document updated

--- randomforest_functions.py
+++ randomforest_functions.py
@@ -1,16 +1,15 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-
 def processamento(file):
+    import pandas as pd
+    
+    
     dados = pd.read_csv(file)
     return dados, dados.info(), dados['Target'].unique(), dados[['Idade na matrícula', 'Taxa de desemprego', 'Taxa de inflação', 'PIB']].describe()
 
 def plot_analises_demograficas(dados):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    
+    
     #Gráficos Demográficos
     sns.displot(dados['Idade na matrícula'], bins=20)
     plt.show()
@@ -22,6 +21,11 @@ def plot_analises_demograficas(dados):
     plt.show()
     
 def plot_analises_economicas(dados):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import plotly.express as px
+    
+    
     color_dict = {'Desistente': 'red', 'Graduado': 'green', 'Matriculado': 'blue'}
     sns.countplot(x='Devedor', hue='Target', data=dados)
     plt.show()
@@ -39,6 +43,10 @@ def plot_analises_economicas(dados):
     
     
 def encoder(dados):
+    import pandas as pd
+    from sklearn.preprocessing import OneHotEncoder
+    
+    
     colunas_categoricas = ['Estado civil', 'Migração', 'Sexo', 'Estrangeiro', 'Necessidades educacionais especiais', 'Devedor', 
                        'Taxas de matrícula em dia', 'Bolsista', 'Curso', 'Período', 'Qualificação prévia']
     encoder = OneHotEncoder(drop='if_binary')
@@ -50,6 +58,9 @@ def encoder(dados):
     return dados_final
 
 def feature_engineering(dados_final):
+    from sklearn.model_selection import train_test_split
+    
+    
     x = dados_final.drop(['Target'], axis=1)
     y = dados_final['Target']
 
@@ -59,6 +70,9 @@ def feature_engineering(dados_final):
     return x_treino, y_treino, x_test, y_test, x_val, y_val
 
 def applying_model(x_treino, y_treino, x_test, y_test, x_val, y_val):
+    from sklearn.ensemble import RandomForestClassifier
+    
+    
     rfmodel1 = RandomForestClassifier(random_state=0, max_depth=5)
     rfmodel1.fit(x_treino, y_treino)
     y_predval = rfmodel1.predict(x_val)
