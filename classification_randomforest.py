81b8fa1b4557137a8b152e0b5c16fa0ea86afeb4
Leonam Alves <leon14alv@gmail.com>
2024-03-21 03:19:32 -0300

    documents updated

--- classification_randomforest.py
+++ classification_randomforest.py
@@ -10,8 +10,8 @@ dados_final = encoder(dados)
 x_treino, y_treino, x_test, y_test, x_val, y_val = feature_engineering(dados_final)
 y_predval, y_predtest, train_accuracy, val_accuracy = applying_model(x_treino, y_treino, x_test, y_test, x_val, y_val)
 cm_val, cm_test, accuracy, precision, recall, f1, report = metrics(y_val, y_predval, y_test, y_predtest)
-print(cm_val)
-print(cm_test)
+print(cm_val) #Matriz de Confusão do conjunto de validação
+print(cm_test) #Matriz de Confusão do conjunto de teste
 print('Acurácia:', accuracy)
 print('Precisão:', precision)
 print('Sensibilidade:', recall)
